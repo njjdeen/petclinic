d561074fedf28998182800af1b1918fcc5490822
Niels Deen <niels.deen@devoteam.com>
2022-03-01 11:39:56 +0100

    added headless option in selenium python script

--- selenium_test.py
+++ selenium_test.py
@@ -8,6 +8,9 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+options = Options()
+options.headless = True
 
 def petclinic_test():
     #initialize driver
