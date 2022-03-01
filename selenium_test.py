2eb059e83930d25b080e6d81c42e2da0ce67434e
Niels Deen <niels.deen@devoteam.com>
2022-03-01 11:48:56 +0100

    edited selenium test

--- selenium_test.py
+++ selenium_test.py
@@ -14,7 +14,7 @@ options.headless = True
 
 def petclinic_test():
     #initialize driver
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome("/usr/bin/chromedriver", Options=options)
 
     driver.get("http://127.0.0.1:9999/petclinic")
 
